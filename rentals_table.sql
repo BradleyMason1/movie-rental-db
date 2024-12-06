6b28c593327149b29a41080f239ef5a84bce5f08
Bradley Mason <157651351+BradleyMason1@users.noreply.github.com>
2024-12-06 13:46:42 -0500

    Update rentals_table.sql

--- rentals_table.sql
+++ rentals_table.sql
@@ -14,7 +14,7 @@ CREATE TABLE rentals (
 -- Inserts rental data
 INSERT INTO rentals (movie_id, customer_id, rental_date, return_date)
 VALUES
-		(1,1, '2024-04-06', '2024-08-06'),   
+	(1,1, '2024-04-06', '2024-08-06'),   
         (2,2, '2023-07-09', '2023-10-08'),
         (3,3, '2024-08-12', '2024-10-11'),
         (4,4, '2020-07-09', '2020-09-12');
